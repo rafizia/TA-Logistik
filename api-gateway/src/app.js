4e9e916aa8a2fd64469bf06db085a3fb4d367614
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2026-05-07 20:43:51 +0700

    fix: try to resolve cors error 4

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -9,17 +9,12 @@ import { swaggerSpec } from "./config/swagger.js";
 
 export const app = express();
 
-const corsOptions = {
-  origin: true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
+app.use(
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
