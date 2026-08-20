f017cd20df4a5381c5d8f190d3d5482f6c1a2c59
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2026-08-20 21:46:54 +0700

    chore: add database seed data

--- api-gateway/src/utils/dataset/do-data.js
+++ api-gateway/src/utils/dataset/do-data.js
@@ -6,7 +6,7 @@ import {
 export const delivery_order_banten = Array.from({ length: 100 }, (_, i) => ({
   so_origin: `PRM/#SO-${(i + 1).toString().padStart(4, "0")}`,
   order_date: "2024-10-22T04:04:46Z",
-  created_by: "admin_id",
+  created_by: "Super",
   loc_ori_id: "54fd3b19-7843-4c8d-a923-82a2be39fb7b",
   loc_dest_id: customer_location_banten[i % customer_location_banten.length].id,
   delivery_order_num: `PRM/#DO-${(i + 1).toString().padStart(4, "0")}`,
@@ -15,7 +15,7 @@ export const delivery_order_banten = Array.from({ length: 100 }, (_, i) => ({
 export const delivery_order_jakarta = Array.from({ length: 20 }, (_, i) => ({
   so_origin: `PRM/#SO-${(i + 101).toString().padStart(4, "0")}`,
   order_date: "2024-10-22T04:04:46Z",
-  created_by: "admin_id",
+  created_by: "Super",
   loc_ori_id: "368a8a8e-e57a-4cc0-8819-7eb9c42e9845",
   loc_dest_id:
     customer_location_jakarta[i % customer_location_jakarta.length].id,
---
+++ api-gateway/src/utils/dataset/static-data/static-data.js
@@ -0,0 +1,6 @@
+export { user } from "./user-data.js";
+export { dclocations } from "./dc-location-data.js";
+export { dcs } from "./dc-data.js";
+export { truck } from "./truck-data.js";
+export { role } from "./role-data.js";
+export { truck_types } from "./truck-type-data.js";
