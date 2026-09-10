95e2bb7ac5693a68662e6fe563f0c02660a8370e
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2026-09-10 22:01:02 +0700

    fix: update env file loc

--- ai-service/config.py
+++ ai-service/config.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(__file__), "..", ".env"),
+        env_file=os.path.join(os.path.dirname(__file__), ".env"),
         env_file_encoding="utf-8",
         extra="ignore",
         populate_by_name=True,
